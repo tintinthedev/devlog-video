b19609275a95c9e596c4151ea1525c583356e309
Timoteo <randomthecoder1557@gmail.com>
2025-09-06 15:36:52 -0300

    rename scene file

--- src/project.ts
+++ src/project.ts
@@ -1,9 +1,9 @@
 import { makeProject } from "@motion-canvas/core";
 
-import example from "./scenes/example?scene";
+import intro from "./scenes/intro?scene";
 import audio from "../audio/audio.mp3";
 
 export default makeProject({
-  scenes: [example],
+  scenes: [intro],
   audio,
 });
